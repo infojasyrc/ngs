9870e104f1834777dc06d3703235534daab0d2e6
Jose Sal y Rosas <infojasyrc@gmail.com>
2019-08-31 12:40:42 -0500

    Solved challenge timer - challenge 01

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,6 +1,9 @@
-const theOneFunc = () => {};
+const theOneFunc = numberOfSeconds => {
+    console.log('Hello after ' + numberOfSeconds+ ' seconds');
+};
 
-setTimeout(theOneFunc, 4 * 1000);
+setTimeout(theOneFunc, 4 * 1000, 4);
+setTimeout(theOneFunc, 8 * 1000, 8);
 
 // Hello after 4 seconds
 
